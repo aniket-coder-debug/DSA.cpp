d8212bd23ca2f4f644403bca2a6ffb140cad6730
Aniket Parbhane <parbhaneaniket53@gmail.com>
2026-01-28 23:55:19 +0530

    Add files via upload

---
+++ client.cpp
@@ -0,0 +1,24 @@
+#include"server.h"
+
+int main(void)
+{
+    int iDelData;
+    queue obj;
+
+    obj.EnQueue(10);
+    obj.EnQueue(20);
+    obj.EnQueue(30);
+    obj.EnQueue(40);
+
+    obj.Display();              //  10 20 30 40
+
+    iDelData = obj.DeQueue();
+    if(iDelData != -1)
+        cout << "\ndeleted data is " << iDelData << endl;       //  deleted data is 10
+
+    obj.EnQueue(50);
+
+    obj.Display();              //  20 30 40 50
+
+    return 0;
+}
